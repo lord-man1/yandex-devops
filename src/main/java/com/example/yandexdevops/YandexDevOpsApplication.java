6a80c25b9c73e5557ecd8c3adaca9b1f2fb8f575
Чижевский Роман Федорович <rchizh@mts.ru>
2025-04-12 19:48:21 +0300

    init

---
+++ src/main/java/com/example/yandexdevops/YandexDevOpsApplication.java
@@ -0,0 +1,13 @@
+package com.example.yandexdevops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YandexDevOpsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YandexDevOpsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/yandexdevops/YandexDevOpsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.yandexdevops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YandexDevOpsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
